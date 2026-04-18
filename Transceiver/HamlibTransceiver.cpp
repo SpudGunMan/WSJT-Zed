ad2c6c7ce3c245a6a4d986e9d95b33982b22a810
Kelly <kellykeeton@yahoo.com>
2026-04-18 14:26:28 -0700

    Update HamlibTransceiver.cpp
    fix compile issue depreciated

--- Transceiver/HamlibTransceiver.cpp
+++ Transceiver/HamlibTransceiver.cpp
@@ -320,7 +320,13 @@ QByteArray HamlibTransceiver::impl::get_conf (char const * item)
   QByteArray value {128, '\0'};
   if (RIG_CONF_END != token)	// only get if valid for rig model
     {
-      error_check (rig_get_conf (rig_.data (), token, value.data ()), tr ("getting a configuration item"));
+#if defined(RIG_FUNCTION_GET_CONF2)
+      error_check (rig_get_conf2 (rig_.data (), token, value.data (), value.size ()),
+                   tr ("getting a configuration item"));
+#else
+      error_check (rig_get_conf (rig_.data (), token, value.data ()),
+                   tr ("getting a configuration item"));
+#endif
     }
   return value;
 }
