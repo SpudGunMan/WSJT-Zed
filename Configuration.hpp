793a34fff3c40820c363dc982992b05cf9ae7845
Kelly <kellykeeton@yahoo.com>
2026-07-20 16:01:48 -0700

    2.0.19-udp1

--- Configuration.hpp
+++ Configuration.hpp
@@ -223,7 +223,6 @@ public:
   bool rxTotxFreq() const;
   bool udpFiltering() const;
   bool highlightDX() const;
-  bool hideOwnCall() const;
   bool dbgScreen () const;
   bool dbgFile () const;
   bool dbgBoth () const;
